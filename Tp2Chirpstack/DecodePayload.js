a6e551d9f891f4862a4232d9848f7fab002dc67a
bouhenic <39024540+bouhenic@users.noreply.github.com>
2024-03-23 12:24:34 +0100

    Update DecodePayload.js

--- Tp2Chirpstack/DecodePayload.js
+++ Tp2Chirpstack/DecodePayload.js
@@ -1,4 +1,4 @@
-function decodeUplink{
+function decodeUplink(input){
 return {
 data: Decode(input.fport,input.bytes,input.variables)
 };
@@ -6,7 +6,7 @@ data: Decode(input.fport,input.bytes,input.variables)
 
 function Decode(fPort,bytes){
 var decoded = {};
-var text = String.fromCharCode.apply(null,bbytes);
+var text = String.fromCharCode.apply(null,bytes);
 decoded.text=text;
 return decoded;
 }
