e1a78ca0d9db1b9de3561dd1ca12d99645a409ad
bouhenic <39024540+bouhenic@users.noreply.github.com>
2024-03-31 14:11:49 +0200

    Update arrosage.js

--- Tp3Ble/arrosage.js
+++ Tp3Ble/arrosage.js
@@ -1,33 +1,32 @@
-// Récupère la valeur d'humidité du payload
-let humidite = msg.payload;
-
-// Définit les seuils pour l'humidité
-const seuilBas = 40;
-const seuilHaut = 80;
-
-// Récupère la valeur du mode du contexte du flux pour déterminer si l'arrosage est géré automatiquement
+// Initialise les variables
+let etatArrosage;
+const seuilBas = 30;
+const seuilHaut = 60;
 let mode = flow.get('mode');
 
-// Initialise une variable pour stocker l'état de l'arrosage
-let etatArrosage;
-
-// Vérifie si le mode automatique est activé
-if (mode === false) {
-    if (humidite < seuilBas) {
-        // Si l'humidité est inférieure à 40% et le mode automatique est activé, déclenche l'arrosage
-        etatArrosage = 1;
-        flow.set("etatArrosage", etatArrosage)
-    } else if (humidite > seuilHaut) {
-        // Si l'humidité est supérieure à 80% et le mode automatique est activé, stoppe l'arrosage
-        etatArrosage = 0;
-        flow.set("etatArrosage", etatArrosage)
-    } else {
-        // Si l'humidité est entre 40% et 80% et le mode automatique est activé, ne change rien
-        etatArrosage = flow.get("etatArrosage");
-    }
+// Vérifie si le payload contient une valeur numérique
+let humidite = parseFloat(msg.payload);
+if (isNaN(humidite)) {
+    // Gère le cas où humidite n'est pas un nombre
+    // Par exemple, définir etatArrosage à une valeur indiquant une erreur ou ignorer la mise à jour
+    etatArrosage = "Erreur: valeur d'humidité non valide";
 } else {
-    // Si le mode automatique n'est pas activé (mode manuel)
-    etatArrosage=flow.get("etatPompe")
+    // Le payload est un nombre, continue avec la logique existante
+    if (mode === false) {
+        if (humidite < seuilBas) {
+            // Si l'humidité est inférieure à 30% et le mode automatique est activé, déclenche l'arrosage
+            etatArrosage = 1;
+        } else if (humidite > seuilHaut) {
+            // Si l'humidité est supérieure à 60% et le mode automatique est activé, stoppe l'arrosage
+            etatArrosage = 0;
+        } else {
+            // Si l'humidité est entre 30% et 60% et le mode automatique est activé, ne change rien
+            etatArrosage = flow.get("state") || "État inconnu"; // Utilise l'état actuel de l'arrosage ou un message par défaut si non disponible
+        }
+    } else {
+        // Si le mode automatique n'est pas activé, pourrait définir etatArrosage à une valeur spécifique ou le laisser indéfini
+        etatArrosage=flow.get('state');
+    }
 }
 
 // Prépare le message de sortie avec l'état de l'arrosage
